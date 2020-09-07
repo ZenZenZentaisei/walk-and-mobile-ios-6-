ea0cc59b7fe8158fc7755c21dd46f8eba550128c
Fujiyama <atsushiaf39@gmail.com>
2020-09-07 16:09:13 +0900

    Initial Commit

---
+++ BrailleBlockRecognition_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrailleBlockRecognition_iOS
+//
+//  Created by matuilab on 2020/09/07.
+//  Copyright © 2020 matuilab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
