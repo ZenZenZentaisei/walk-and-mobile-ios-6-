de59219dca310dae984c076fa717b9993c64514e
r0227n <b1803518@planet.kanazawa-it.ac.jp>
2021-05-17 11:46:06 +0900

    プログラム全てをgitで管理

---
+++ BBlock_en+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  BBlock_en+CoreDataClass.swift
+//  
+//
+//  Created by matuilab on 2020/12/07.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(BBlock_en)
+public class BBlock_en: NSManagedObject {
+
+}
